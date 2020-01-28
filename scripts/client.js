e14f6557433c2ca68330d01272c2b3f351a4505d
Dev Jana <devjana@Devs-MacBook-Pro-2.local>
2020-01-28 11:00:43 -0600

    end of lecture

--- scripts/client.js
+++ scripts/client.js
@@ -13,5 +13,30 @@ function arrayFromIndex( myArray, startingIndex ){
     } // end for
     return stringToReturn;
 } // end func
+
+// Create a function that takes in an array of employee objects.
+// Each employee object will come in with a property called yearsOfExperience.
+// Your function should return the total years of experience
+// for all of the employees.
+
+function totalyearsExperience( employeesArray ){
+    console.log( 'in totalyearsExperience:', employeesArray );
+    // create a new cariable to hold sum of all experience
+    let yearsToReturn = 0;
+    // loop through array
+    for( let i=0; i < employeesArray.length; i++ ){
+        // total all yearsOfExperience
+        yearsToReturn += employeesArray[ i ].yearsOfExperience;
+    } //end for
+    // return totalyearsExperience
+    return yearsToReturn;
+} // end func
+
 // run tests
-console.log( arrayFromIndex( [ 'asdf', 'qwer', 'zxcv', '1234' ], 2 ) );
+console.log( totalyearsExperience( [
+    { name: "Tony", yearsOfExperience: 10 }, 
+    { name: "Carla", yearsOfExperience: 4 },
+    { name: "Kris", yearsOfExperience: 14 },
+    { name: "Casie", yearsOfExperience: 7000000 }
+] ) );
+// console.log( arrayFromIndex( [ 'asdf', 'qwer', 'zxcv', '1234' ], 2 ) );
